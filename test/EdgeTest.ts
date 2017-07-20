188cfde8a33ca0ec8cdef2b6037a799cf4d38ab5
eakarpov <eakarpov@yandex.ru>
2017-07-20 18:21:48 +0300

    Edge test completed

--- test/EdgeTest.ts
+++ test/EdgeTest.ts
@@ -2,11 +2,35 @@ import * as chai from "chai";
 import {Vertex} from "../src/main/Vertex";
 import {Edge} from "../src/main/Edge";
 describe('Edge', () => {
-   describe('#isIncident();', () => {
-       it('Vertex should return the vertex in the edge', () => {
+    const vertex = new Vertex("my_name");
+    const vertexOne = new Vertex("first");
+    const vertexTwo = new Vertex("second");
+    const edgeWithVertex = new Edge(vertex, vertexTwo);
+    const edgeWithoutVertex = new Edge(vertexOne, vertexTwo);
+
+    describe('#isIncident();', () => {
+       it('Vertex of the edge should be incident to it', () => {
+          chai.assert(edgeWithVertex.isIncident(vertex) == true);
+       });
+
+       it('Vertex not of the edge should not be incident to it', () => {
           const vertex = new Vertex("my_name");
-          const edge = new Edge(vertex, new Vertex("another vertex"));
-          chai.assert(edge.isIncident(vertex) == true);
+          chai.assert(edgeWithoutVertex.isIncident(vertex) == false);
        });
    });
+
+   describe('#equals();', () => {
+       it('If functions gets null it responses with false answer', () => {
+          chai.assert(edgeWithVertex.equals(null) == false);
+       });
+
+       it('If the received edge has the same vertexOne and vertexTwo, it is equal', () => {
+           const edge = new Edge(vertexOne, vertexTwo);
+           chai.assert(edgeWithoutVertex.equals(edge) == true);
+       });
+
+       it('If one of the vertices in the received edge is not the same, edges are not equal', () => {
+          chai.assert(edgeWithoutVertex.equals(edgeWithVertex) == false);
+       });
+   })
 });
