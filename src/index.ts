2d82bb177eaf78d131c13cb103478f830985087d
eakarpov <eakarpov@yandex.ru>
2017-08-22 15:41:07 +0300

    Export configured

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ export * from './types/IEdge';
 export * from './main/DirectedEdge';
 export * from './main/UndirectedEdge';
 export * from './main/DirectedWeightedEdge';
+export * from './types/IWeightedEdge';
 
+export * from './types/IGraphStatic';
 export * from './types/IGraph';
 export * from './main/Graph';
 export * from './main/UndirectedGraph';
@@ -12,3 +14,17 @@ export * from './main/DirectedWeightedGraph';
 
 export * from './types/IVertex';
 export * from './main/Vertex';
+
+export * from './types/EdgeJSON';
+export * from './types/GraphJSON';
+export * from './types/VertexJSON';
+
+export * from './serializers/EdgeToJSONConverter';
+export * from './serializers/GraphSerializer';
+export * from './serializers/GraphToJSONConverter';
+export * from './serializers/VertexToJSONConverter';
+
+export * from './helpers/GraphGenerator';
+
+export * from './enums/StateColor';
+export * from './util/GraphID';
