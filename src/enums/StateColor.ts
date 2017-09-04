645c92257f068c56d2912949c4d7d9791bfab5d4
George <eakarpov@yandex.ru>
2017-09-04 12:39:27 +0300

    Fixed bug with necessary enum StateColor

---
+++ src/enums/StateColor.ts
@@ -0,0 +1,17 @@
+/**
+ * Vertex colors for dominating sets algorithm
+ */
+export enum StateColor {
+        /**
+         * White color
+         */
+    WHITE,
+        /**
+         * Blue color
+         */
+    BLUE,
+        /**
+         * Red color
+         */
+    RED
+}
