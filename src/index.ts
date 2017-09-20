7d672f0c1b0dedccc4b07146612ffb87524d3584
George <eakarpov@yandex.ru>
2017-09-20 23:24:16 +0300

    more exports permitted

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,21 @@
 export * from './main/Edge';
-export * from './types/IEdge';
 export * from './main/DirectedEdge';
 export * from './main/UndirectedEdge';
 export * from './main/DirectedWeightedEdge';
-export * from './types/IWeightedEdge';
-
-export * from './types/IGraphStatic';
-export * from './types/IGraph';
 export * from './main/Graph';
 export * from './main/UndirectedGraph';
 export * from './main/DirectedGraph';
 export * from './main/DirectedWeightedGraph';
-
-export * from './types/IVertex';
 export * from './main/Vertex';
 
 export * from './types/EdgeJSON';
 export * from './types/GraphJSON';
 export * from './types/VertexJSON';
+export * from './types/IEdge';
+export * from './types/IVertex';
+export * from './types/IWeightedEdge';
+export * from './types/IGraphStatic';
+export * from './types/IGraph';
 
 export * from './serializers/EdgeToJSONConverter';
 export * from './serializers/GraphSerializer';
@@ -27,3 +25,11 @@ export * from './serializers/VertexToJSONConverter';
 export * from './helpers/GraphGenerator';
 
 export * from './util/GraphID';
+export * from './util/CheckSet';
+export * from './util/State';
+
+export * from './enums/StateColor';
+
+export * from './algorithms/IsomorphismChecker';
+export * from './algorithms/MinDSEvaluator';
+export * from './algorithms/SccBuilder';
