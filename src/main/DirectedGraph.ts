ec5715077af7b0c78f83d4fe7b1067cb678128c7
atitova <alex.n.titova@gmail.com>
2022-05-17 18:14:45 +0300

    add new realisation at methods

--- src/main/DirectedGraph.ts
+++ src/main/DirectedGraph.ts
@@ -6,6 +6,7 @@ import {UndirectedGraph} from "./UndirectedGraph";
 import {SccBuilder} from "../algorithms/SccBuilder";
 import {IGraph} from "../types/IGraph";
 import {IEdge} from "../types/IEdge";
+import {forEach} from "lodash";
 
 /**
  * Directed graph
@@ -16,8 +17,8 @@ export class DirectedGraph extends Graph<Vertex, DirectedEdge> {
   /**
    * @constructor
    */
-  public constructor() {
-    super(true);
+  public constructor(directed: boolean = true) {
+    super(directed);
   }
 
   /**
@@ -26,8 +27,24 @@ export class DirectedGraph extends Graph<Vertex, DirectedEdge> {
    * @param vertexTwo
    * @returns {DirectedEdge}
    */
-  public getEdge(vertexOne: IVertex, vertexTwo: IVertex): DirectedEdge[] {
-    return this.edges.filter(a => a.vertexTwo.equals(vertexTwo) && a.vertexOne.equals(vertexOne));
+  public getEdge(vertexOne: IVertex, vertexTwo: IVertex): DirectedEdge[]{
+
+    let e:DirectedEdge;
+
+    let i:number=0;
+    let result:DirectedEdge[] =[];
+
+    for (i=0; i<this.edges.length; i++)
+    {
+      if (this.edges[i].vertexOne.name === vertexOne.name && this.edges[i].vertexTwo.name ===vertexTwo.name)
+      {
+        result.push(new DirectedEdge(this.edges[i].vertexOne, this.edges[i].vertexTwo, this.edges[i].name, this.edges[i].weightLabel, this.edges[i].isDirected));
+      }
+    }
+
+    // return this.edges.filter(a => a.vertexTwo.name === vertexTwo.name && a.vertexOne.name == vertexOne.name);
+
+     return result;
   }
 
   public buildSCC(): IGraph<IVertex, IEdge>[] {
@@ -39,13 +56,13 @@ export class DirectedGraph extends Graph<Vertex, DirectedEdge> {
    * @returns {DirectedGraph}
    */
   public clone(): DirectedGraph {
-    const clone = new DirectedGraph();
+    const clone = new DirectedGraph(true);
     this.vertices.forEach(v => clone.addVertex(v.clone()));
     for (const edge of this.edges)
     {
-      const v1 = clone.vertices.filter(edge.vertexOne.equals)[0]; //Single?
-      const v2 = clone.vertices.filter(edge.vertexTwo.equals)[0];
-      clone.addEdge(new DirectedEdge(v1, v2));
+      // const v1 = clone.vertices.filter(edge.vertexOne.equals)[0]; //Single?
+      // const v2 = clone.vertices.filter(edge.vertexTwo.equals)[0];
+      clone.addEdge(new DirectedEdge(edge.vertexOne, edge.vertexTwo, edge.name, edge.weightLabel, edge.isDirected));
     }
 
     return clone;
